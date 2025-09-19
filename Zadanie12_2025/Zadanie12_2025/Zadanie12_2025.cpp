c5a58b235517ee50223e984d656e38b4bc68e68f
kubabb <kubarogoz3@gmail.com>
2025-09-19 22:06:39 +0200

    zadanie 13 z wyjasnieniem, zadanie 12 do zrobienia na pozniej

---
+++ Zadanie12_2025/Zadanie12_2025/Zadanie12_2025.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+#include <cstdint>
+
+int f(int num) {
+    std::uint8_t a = num & 0x1F;
+    std::uint8_t b = ((a << 2) | 0x03) ^ 0xA5;
+    std::uint8_t c = (b % 26);
+    return c;
+}
+
+int main()
+{
+    
+    cout << f(11) << endl; 
+    cout << f(305) << endl;
+    cout << f(557) << endl;
+    cout << f(1101) << endl;
+
+
+
+}
+
---
+++ Zadanie13_2025/Zadanie13_2025/Zadanie13_2025.cpp
@@ -0,0 +1,30 @@
+﻿using namespace std;
+#include <iostream>
+
+// trudnosc to zrozumienie przeliczania liczb w systemach, do powtorzenia. Odpowiedz foo(0x5e) 5 X 16 + 14 X 1
+//Funkcja foo() liczy liczbę jedynek(bitów ustawionych na 1) w liczbie całkowitej n.
+// Działa na zasadzie sprawdzania każdego bitu z osobna — od najmłodszego do najstarszego.
+// sizeof(int) << 3 oblicza liczbę bitów w typie int (np. 4 bajty × 8 = 32 bity).
+// Uwaga: kod nie jest optymalny — zawsze wykonuje pełne 32 iteracje, nawet dla małych liczb.
+
+int foo(int n) {
+    int a = 0;
+    int b = sizeof(int) << 3;
+    for (int i = 0; i < b; i++) {
+        if (n & (1 << i)) a++;
+    }
+    return a;
+}
+
+
+int main()
+{
+	cout << foo(94) << endl;
+    cout << foo(50) << endl;
+    cout << foo(0175) << endl;
+    cout << foo(1101) << endl;
+      
+}
+
+
+
